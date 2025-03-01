1ebf06306bc25be73b0ff5106eccb0987257e98c
gdillaman <gdillaman@gmail.com>
2025-02-28 21:52:53 -0500

    Updating to allow github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/YOUR_REPOSITORY_NAME/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base:"/"
 })

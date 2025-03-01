0c584f1a77d252aa83a9924caf54eebaac4e6f0b
gdillaman <gdillaman@gmail.com>
2025-02-28 22:00:51 -0500

    Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/YOUR_REPOSITORY_NAME/',
+  base: '/JSONFormatter/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
